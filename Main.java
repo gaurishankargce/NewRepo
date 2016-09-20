0234055c64941e6cc74157624b8e91e1e9997797
gaurishankargce <gaurishankargce45@gmail.com>
2016-09-20 11:18:23 +0530

    modifying from remote

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main
 	{
 		System.out.println("Hello World!");
         System.out.println("hello github.com,modifying file");
-
+       System.out.println("hello github.com,modifying file from remote");
 	}
 }
