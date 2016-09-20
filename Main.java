bd719866e6a771229e52823caf9491b8f97992cb
gaurishankargce <gaurishankar45@gmail.com>
2016-09-20 23:46:11 +0530

    pulling remote branch to local repository.

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main
         System.out.println("hello github.com,modifying file");
        System.out.println("hello github.com,modifying file from remote");
 	    System.out.println("hello github.com,creating branch from local");
+        System.out.println("hello github.com,pulling remote branch to local");
   
 	}
 }
